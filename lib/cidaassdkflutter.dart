434eff74f3c82a4c414107489f2b941eb8ccd375
Florian Grossmann <florian.grossmann@widas.de>
2020-06-10 16:54:39 +0200

    exported TokenEntity

--- lib/cidaassdkflutter.dart
+++ lib/cidaassdkflutter.dart
@@ -3,3 +3,4 @@ library cidaassdkflutter;
 export './src/de/cidaas/sdk/flutter/cidaas_login_provider.dart';
 export './src/de/cidaas/sdk/flutter/cidaas.dart';
 export './src/de/cidaas/sdk/flutter/authentification/authentication_bloc.dart';
+export './src/de/cidaas/sdk/flutter/entity/token_entity.dart';
