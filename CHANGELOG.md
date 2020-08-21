35a7e18becee294e8661878c6ccbbc582e3dc700
Max Kuhnle <max.kuhnle@widas.de>
2020-08-21 15:09:42 +0200

    Revert "1.0.0"
    This reverts commit c69a18e2cb1a135cfbbd9935b04ed3eb8d91eb29

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,3 @@
 # Release notes cidaas Flutter SDK
-## [1.0.0] - 26.07.2020
-First version release
-## [1.0.0-dev.2] - 25.07.2020
-formatted, linting & type safety
 ## [1.0.0-dev.1] - 25.07.2020
 initial setup flutter sdk
--- lib/cidaas_flutter_sdk.dart
+++ lib/cidaas_flutter_sdk.dart
@@ -1,7 +1,7 @@
 /// Provides the necessary logic to authenticate users with cidaas, using the oAuth2 Authorization code flow
 library cidaas_flutter_sdk;
 
-export './src/de/cidaas/sdk/flutter/authentification/authentication_bloc.dart';
-export './src/de/cidaas/sdk/flutter/cidaas.dart';
 export './src/de/cidaas/sdk/flutter/cidaas_login_provider.dart';
+export './src/de/cidaas/sdk/flutter/cidaas.dart';
 export './src/de/cidaas/sdk/flutter/entity/entities.dart';
+export './src/de/cidaas/sdk/flutter/authentification/authentication_bloc.dart';
